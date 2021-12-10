64588a3cb6068470b76ca1904b91b0ab18eb4c5b
rniemczura1228 <94641599+rniemczura1228@users.noreply.github.com>
2021-12-10 15:51:22 -0500

    Update test_obj_or.py

--- test_obj_or.py
+++ test_obj_or.py
@@ -1,15 +1,33 @@
 def total(a,b):
-   global result
-   result=a+b 
+    global result
+    result=a+b 
+
+def openFile(filename,mode):
+    global f
+    f = open(filename,mode)
+
+def writeLine(line):
+    f.write(line)
+
+def closeFile():
+    f.close()
 
 def main():
-    print("Hello, World!")
-    a= int(input("Enter first number:"))
-    b= int(input("Enter second number:"))
-    total(a,b)#funtion call the 2 integer
+
+    # Number Function
+    a = 5
+    b = 7
+    total(a,b)
     print(" Sum of ",a,"and"," sun of ",b,"result ", result)
-    total(5,7)
-    print(" Sum of ",a,"and"," sun of ",b,"result ", result)
+
+    # Write to file
+    filename = "workfile.txt"
+    mode = "a+"
+    openFile(filename,mode)
+    for i in range(10):
+       line = "This is line " + str(i) + "\n"
+       writeLine(line)
+    closeFile()
     
 if __name__== "__main__" :
         main()
