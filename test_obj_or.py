f7e88caf3635339f073da9f10f07c19bbb6afd21
rniemczura1228 <94641599+rniemczura1228@users.noreply.github.com>
2021-12-10 11:08:33 -0500

    Update test_obj_or.py

--- test_obj_or.py
+++ test_obj_or.py
@@ -1,5 +1,15 @@
+def total(a,b):
+   global result
+   result=a+b 
+
 def main():
     print("Hello, World!")
+    a= int(input("Enter first number:"))
+    b= int(input("Enter second number:"))
+    total(a,b)#funtion call the 2 integer
+    print(" Sum of ",a,"and"," sun of ",b,"result ", result)
+    total(5,7)
+    print(" Sum of ",a,"and"," sun of ",b,"result ", result)
     
 if __name__== "__main__" :
         main()
