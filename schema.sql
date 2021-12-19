298add3f9a6b9b24f22e36ab1e65784efaf3b8cf
rniemczura1228 <94641599+rniemczura1228@users.noreply.github.com>
2021-12-19 16:18:08 -0500

    Delete schema.sql

--- schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists posts;
-
-create table posts (
-     id integer primary key autoincrement,
-     name varchar(64),
-     number_parts integer,
-     created_date varchar(24),
-     updated_date varchar(24)
-);
